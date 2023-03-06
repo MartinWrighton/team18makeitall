3d4c67359a12f321bfddb08a48ab74b16c3f9625
MartinWrighton <martin.wrighton@outlook.com>
2023-03-06 17:08:29 +0000

    Prototype Messenger

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Make-It-All
 Team 18 group project part 2
+
+To run this you will need to have Node installed.
+
+When you first clone this, open an terminal and run: npm install.
+
+Then to run the website run: npm run dev
+
+It will load on http://localhost:3000
---
+++ pages/index.tsx
@@ -0,0 +1,50 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
+import Router from 'next/router'
+import { useState } from 'react'
+
+
+export default function Home() {
+  const [userID, setuserID] = useState("")
+
+
+
+
+
+
+
+
+  return (
+    <>
+      <Head>
+        <title>Team 18</title>
+        <meta name="description" content="Team 18 part 3" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className='w-screen h-screen pt-5 bg-blue-200'>
+        <div className='mx-auto p-5 bg-white shadow-xl rounded-xl w-1/3 h-fit'>
+          <h1 className='text-center font-bold font-sans text-5xl'>Team 18 Part 3</h1>
+        </div>
+        <div className='mx-auto my-5 p-5 bg-white shadow-xl rounded-xl w-8/12 h-5/6 space-y-5'>
+          <h1 className='text-center font-bold font-sans text-3xl'>Log In</h1>
+          <div className='w-fit mx-auto space-y-5'>
+            <label>Name: </label>
+            <input id="name"  placeholder='Name' className='justify-center border-2 border-blue-300 rounded-md bg-gray-100' onInput={(e:any)=>{
+              setuserID(e.target.value!)
+            }
+            }></input>
+            <br/>
+            <div className='w-fit mx-auto'>
+             <button className='bg-blue-500 p-5 rounded-lg h-fit  w-fit mx-auto font-bold font-sans text-xl' onClick={(e)=>{
+                Router.push("./messenger?userID="+userID)
+             }}>Log In</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
